aab46605981f0f40ce4ee716f6d33f066fcab918
agrawalvedansh <agrawal.vedansh@gmail.com>
2024-08-06 18:37:20 +0530

    Create custom hook for debouncing

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import productsApi from "apis/product";
 import { Header, PageLoader } from "components/commons";
 import ProductListItem from "components/ProductList/ProductListItem";
+import useDebounce from "hooks/useDebounce";
 import { Search } from "neetoicons";
 import { Input, NoData } from "neetoui";
 import { isEmpty } from "ramda";
@@ -11,10 +12,13 @@ const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [searchKey, setSearchKey] = useState("");
+  const debouncedSearchKey = useDebounce(searchKey);
 
   const fetchProducts = async () => {
     try {
-      const { products } = await productsApi.fetch({ searchTerm: searchKey });
+      const { products } = await productsApi.fetch({
+        searchTerm: debouncedSearchKey,
+      });
       setProducts(products);
     } catch (error) {
       console.log("An error occurred:", error);
@@ -25,7 +29,7 @@ const ProductList = () => {
 
   useEffect(() => {
     fetchProducts();
-  }, [searchKey]);
+  }, [debouncedSearchKey]);
 
   if (isLoading) {
     return <PageLoader />;
