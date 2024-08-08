49de1f8aed1f07a228681cab0ee02bf186fa490f
agrawalvedansh <agrawal.vedansh@gmail.com>
2024-08-08 22:47:34 +0530

    Add ability to set quantity of cart items

---
+++ src/components/commons/TooltipWrapper.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import { Tooltip } from "neetoui";
+
+const TooltipWrapper = ({ showTooltip, children, ...tooltipProps }) => {
+  if (!showTooltip) return children;
+
+  return (
+    <Tooltip {...tooltipProps}>
+      <div>{children}</div>
+    </Tooltip>
+  );
+};
+
+export default TooltipWrapper;
